f45361d7b2f999d659008cb94710738d65f5b405
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-10-23 01:04:20 -0500

    part 1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
+import { App } from "./App.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-function Componente({ titulo, children }) {
-  // const titulo = props.titulo
-  // const contenido = props.contenido
-  // const { titulo, contenido } = props
-  return (
-    <div className="contenedor">
-      <h1>{titulo}</h1>
-      {/* <div>{contenido}</div> */}
-      <div>{children}</div>
-    </div>
-  )
-}
-root.render(
-  <Componente titulo="Titulo">Hola desde react</Componente>,
-  document.getElementById("root")
-)
+root.render(<App />, document.getElementById("root"))
