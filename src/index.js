2bd1042cbcc32f5bf7b40a24459d8a77cd1fb632
David Julian Jaramillo <davidjulianjaramillo@gmail.com>
2022-10-22 23:56:59 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+function Componente({ titulo, children }) {
+  // const titulo = props.titulo
+  // const contenido = props.contenido
+  // const { titulo, contenido } = props
+  return (
+    <div className="contenedor">
+      <h1>{titulo}</h1>
+      {/* <div>{contenido}</div> */}
+      <div>{children}</div>
+    </div>
+  )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Componente titulo="Titulo">Hola desde react</Componente>,
+  document.getElementById("root")
+)
